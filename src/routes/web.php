10b6265a70752ff583c001176e1a29bb8eba685f
coffee-r <game.developer.ryo@gmail.com>
2024-11-03 23:13:19 +0900

    ストアド練習

--- src/routes/web.php
+++ src/routes/web.php
@@ -2,6 +2,16 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+//Route::get('/', function () {
+//    return view('welcome');
+//});
+
+Route::get('/stored/{userId}', function ($userId) {
+    $outputEmail = '';
+
+    return \Illuminate\Support\Facades\DB::select('
+        DECLARE @out NVARCHAR(255);
+        EXEC GetUserEmailById ?, @out OUTPUT;
+        SELECT @out as out;
+    ', [$userId]);
 });
